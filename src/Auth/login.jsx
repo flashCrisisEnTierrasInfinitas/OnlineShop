77328f5bf9cf56b3114bc8b458dfc03236ffe841
flashCrisisEnTierrasInfinitas <desarrollo3@ingeal.com>
2023-09-03 23:22:37 -0500

    fix.login

--- src/Auth/login.jsx
+++ src/Auth/login.jsx
@@ -1,19 +1,48 @@
 import { CFormInput } from "@coreui/react";
 import { useState } from "react";
+import Swal from "sweetalert2";
 
 export default function Login() {
-    const [formData, setFormData] = useState({
+
+    const [log, setLog] = useState(false)
+    //VALORES QUE SE ENVIAN EN EL AXIOS
+    const [dataLog, setDataLog] = useState({
         Usuario: "",
         Password: "",
     });
-
     const handleChange = (e) => {
         const { name, value } = e.target;
-        setFormData((prevData) => ({
+        setDataLog((prevData) => ({
             ...prevData,
             [name]: value,
         }));
     };
+    //REGISTRO DEL USUARIO
+    const RegLog = () => {
+        if (!dataLog.Usuario | dataLog.Password) {
+            return Swal.fire({
+                position: 'top-end',
+                icon: 'error',
+                title: 'Todos los campos son requeridos',
+                showConfirmButton: false,
+                timer: 1900
+            })
+        } else {
+            Swal.fire({
+                position: 'top-end',
+                icon: 'success',
+                title: 'Registro exitoso',
+                showConfirmButton: false,
+                timer: 1900
+            })
+            return window.location.replace("/");
+        }
+    }
+    //INGRESO DEL USUARIO
+    const Sing = () => {
+
+    }
+
 
     return (
         <div className="conted-login grid">
@@ -28,13 +57,13 @@ export default function Login() {
                     </div>
                 </div>
                 <div className="box-login">
-                    <form className="form-login">
-                        <h1 className="top-50">Ingresar</h1>
+                    {log ? (<form className="form-login">
+                        <h1 className="top-50">Registrarse</h1>
                         <CFormInput
                             placeholder="Usuario"
                             className="inpunt-login"
                             name="Usuario"
-                            value={formData.Usuario}
+                            value={dataLog.Usuario}
                             onChange={handleChange}
                         />
                         <CFormInput
@@ -42,7 +71,33 @@ export default function Login() {
                             className="inpunt-login"
                             type="Password"
                             name="Password"
-                            value={formData.Password}
+                            value={dataLog.Password}
+                            onChange={handleChange}
+                        />
+                        <div className="conte-terminos">
+                            <div>
+                                <input type="checkbox" checked />
+                                <label className="color-gray">Acepta términos y condiciones</label>
+                            </div>
+                        </div>
+                        <div className="flex">
+                            <button type="button" className="btn1 btn-primary" onClick={() => RegLog()}>Registrarse</button>
+                        </div>
+                    </form>) : (<form className="form-login">
+                        <h1 className="top-50">Ingresar</h1>
+                        <CFormInput
+                            placeholder="Usuario"
+                            className="inpunt-login"
+                            name="Usuario"
+                            value={dataLog.Usuario}
+                            onChange={handleChange}
+                        />
+                        <CFormInput
+                            placeholder="Password"
+                            className="inpunt-login"
+                            type="Password"
+                            name="Password"
+                            value={dataLog.Password}
                             onChange={handleChange}
                         />
                         <div className="conte-terminos">
@@ -54,8 +109,12 @@ export default function Login() {
                                 <label>¿Olvido su contraseña?</label>
                             </div>
                         </div>
-                        <button type="button" className="btn1 btn-secondary">Ingresar</button>
-                    </form>
+                        <div className="flex">
+                            <button type="button" className="btn1 btn-primary" onClick={() => setLog(true)}>Registrarse</button>
+                            <button type="button" className="btn1 btn-secondary">Ingresar</button>
+                        </div>
+                    </form>)}
+
                 </div>
             </div>
         </div>
