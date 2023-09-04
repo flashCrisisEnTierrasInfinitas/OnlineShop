911ef82c66bdeddb0fc71aa4c2febd2d93b7cea3
flashCrisisEnTierrasInfinitas <desarrollo3@ingeal.com>
2023-09-03 22:38:35 -0500

    login:create

--- src/Auth/login.jsx
+++ src/Auth/login.jsx
@@ -14,31 +14,47 @@ export default function Login() {
             [name]: value,
         }));
     };
-    console.log(formData);
+
     return (
         <div className="conted-login grid">
             <div className="conter-box-login grid">
                 <div className="wellcome">
-
+                    <div className="text-wellcome">
+                        <a>
+                            <p>WELCOME</p>
+                            <label>ASUPRO</label>
+                            <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum molestiae aspernatur nisi, reiciendis</h3>
+                        </a>
+                    </div>
                 </div>
                 <div className="box-login">
                     <form className="form-login">
-                        <h1 className="top-50 color-primary">Iniciar</h1>
+                        <h1 className="top-50">Ingresar</h1>
                         <CFormInput
                             placeholder="Usuario"
-                            className="input-search"
+                            className="inpunt-login"
                             name="Usuario"
                             value={formData.Usuario}
                             onChange={handleChange}
                         />
                         <CFormInput
                             placeholder="Password"
-                            className="input-search"
+                            className="inpunt-login"
                             type="Password"
                             name="Password"
                             value={formData.Password}
                             onChange={handleChange}
                         />
+                        <div className="conte-terminos">
+                            <div>
+                                <input type="checkbox" checked />
+                                <label className="color-gray">Acepta términos y condiciones</label>
+                            </div>
+                            <div>
+                                <label>¿Olvido su contraseña?</label>
+                            </div>
+                        </div>
+                        <button type="button" className="btn1 btn-secondary">Ingresar</button>
                     </form>
                 </div>
             </div>
