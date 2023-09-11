5f088ac8087c20b879ce61aaefe437d78c536416
flashCrisisEnTierrasInfinitas <desarrollo3@ingeal.com>
2023-09-11 02:00:52 -0500

    update

--- src/content/admin/dahsboard/index.jsx
+++ src/content/admin/dahsboard/index.jsx
@@ -1,14 +1,24 @@
 import ContedAdmin from "../components/Conter";
 import BarChart from "./BarChart";
+import CWidgetStatsD from "./CWidgetStatsD";
+import Doughnut from "./Doughnut ";
 
 export default function DahsboardAdmin() {
   return (
     <ContedAdmin>
-        <h1>dahsboard</h1>
-        <div className="barChar">
-        <BarChart/>
+      <h1>dahsboard</h1>
+
+      <div className="box-admin">
+        <CWidgetStatsD />
+      </div>
+      <div className="box-cont-admin">
+        <div className="box-admin">
+          <BarChart />
         </div>
-       
+        <div className="box-admin">
+          <Doughnut />
+        </div>
+      </div>
     </ContedAdmin>
   );
 }
