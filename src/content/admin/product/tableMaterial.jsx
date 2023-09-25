9914bd02e32923606ced36b9fe6da85fcde1bb78
Jhon Mario Chilito <desarrollo3@ingeal.com>
2023-09-24 19:14:40 -0500

    gfh

--- src/content/admin/product/tableMaterial.jsx
+++ src/content/admin/product/tableMaterial.jsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { DataGrid } from '@mui/x-data-grid';
+import { DataGrid, GridToolbar } from '@mui/x-data-grid';
 import { CFormInput, CSpinner } from '@coreui/react';
 import isMountedRef from '../../../hooks/useRefMounted'
 import axios from 'axios';
