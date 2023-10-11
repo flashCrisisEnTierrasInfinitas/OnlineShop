90f692e8d775b0ffbbc2ae744cad60e490ace114
flashCrisisEnTierrasInfinitas <desarrollo3@ingeal.com>
2023-10-10 19:54:37 -0500

    gfhgf

--- src/content/pages/home/Conted.jsx
+++ src/content/pages/home/Conted.jsx
@@ -1,9 +1,25 @@
+import { Button } from "@mui/material";
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
 export default function Conted() {
-    return (
-        <div className="resul-home">
-            <div className="conter-img-home">
-                <img src="img/vegetable_shop_tomato_9249.jpg"/>
-            </div>
+  return (
+    <div className="resul-home">
+      <div className="text-resul">
+        <h1>Asupro Colombia</h1>
+        <p>Venta de productos de la canasta familiar, entre otros.</p>
+        <div className="boton-product">
+          <Button variant="contained">Login</Button>
+          <Button variant="outlined">Empecemos <ExpandMoreIcon/></Button>
         </div>
-    )
+      </div>
+      <div className="img-resul">
+        <div>
+          <img
+            src="/img/backgroud/undraw_shopping_app_flsj.svg"
+            alt="img-resul"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
