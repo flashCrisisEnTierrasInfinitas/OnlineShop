4961e789be295010988f3f976a1538ff03bdc7de
Jhon Mario Chilito <desarrollo3@ingeal.com>
2023-11-01 17:34:52 -0500

    fdg

--- src/content/pages/store/detalle/index.js
+++ src/content/pages/store/detalle/index.js
@@ -2,13 +2,7 @@ import { useCallback, useEffect, useState } from "react";
 import isMountedRef from "../../../../hooks/useRefMounted";
 import { CSpinner } from "@coreui/react";
 import axios from "axios";
-import Alert from "@mui/joy/Alert";
-import AspectRatio from "@mui/joy/AspectRatio";
-import IconButton from "@mui/joy/IconButton";
-import LinearProgress from "@mui/joy/LinearProgress";
-import Typography from "@mui/joy/Typography";
-import WarningIcon from "@mui/icons-material/Warning";
-import Close from "@mui/icons-material/Close";
+import WestIcon from "@mui/icons-material/West";
 import { useParams } from "react-router-dom";
 import Swal from "sweetalert2";
 
@@ -100,49 +94,13 @@ export default function DetalleProduc({
   return (
     <div className="conter-detallepro">
       <header>
-        <div>
-          <Alert
-            size="lg"
-            color="success"
-            variant="solid"
-            invertedColors
-            startDecorator={
-              <AspectRatio
-                variant="solid"
-                ratio="1"
-                sx={{
-                  minWidth: 40,
-                  borderRadius: "50%",
-                  boxShadow: "0 2px 12px 0 rgb(0 0 0/0.2)",
-                }}
-              >
-                <div>
-                  <WarningIcon fontSize="xl2" />
-                </div>
-              </AspectRatio>
-            }
-            endDecorator={
-              <IconButton
-                variant="plain"
-                sx={{
-                  "--IconButton-size": "32px",
-                  transform: "translate(0.5rem, -0.5rem)",
-                }}
-              >
-                <Close />
-              </IconButton>
-            }
-            sx={{ alignItems: "flex-start", overflow: "hidden" }}
-          >
-            <div>
-              <Typography level="title-lg">Warning</Typography>
-              <Typography>
-                Este producto se cobra por peso. El precio total en esta orden
-                es estimado y dependerá del peso final al momento de preparar su
-                orden.
-              </Typography>
-            </div>
-          </Alert>
+        <div className="img-detalle">
+          <div className="ico-detalle">
+            <a href="/">
+              <WestIcon />
+            </a>
+          </div>
+          <img src={data.img} alt={data.nombrePro} />
         </div>
         <h1 className="title-detalle">{data.nombrePro}</h1>
         <p className="text-detalle">
@@ -155,9 +113,7 @@ export default function DetalleProduc({
           <strong>Precio:</strong> ${data.precioPro.toLocaleString("es-CO")}
         </p>
       </header>
-      <div className="img-detalle">
-        <img src={data.img} alt={data.nombrePro} />
-      </div>
+
       <div className="descrip-detalle">
         <h3 className="color-gray">{data.descripPro}</h3>
       </div>
