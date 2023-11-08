4230cbca83dac05cbd105ce7dd9c8b52b6167a4e
Jhon Mario Chilito <desarrollo3@ingeal.com>
2023-11-07 21:56:44 -0500

    dfgh

--- src/content/admin/dahsboard/edit.jsx
+++ src/content/admin/dahsboard/edit.jsx
@@ -23,68 +23,40 @@ import { useEffect, useState } from "react";
 import Swal from "sweetalert2";
 
 export default function Edit({ data }) {
-
   const [visible, setVisible] = useState(false);
 
   const Status = ({ data }) => {
-
     if (data == 0) {
       return (
-        <input
-          type="text"
-          class="form-control"
-          value='sin entrega'
-          disabled
-        />);
-    } if (data == 1) {
+        <input type="text" class="form-control top-20" value="sin entrega" disabled />
+      );
+    }
+    if (data == 1) {
       return (
-        <input
-          type="text"
-          class="form-control"
-          value='rechazado'
-          disabled
-        />);
+        <input type="text" class="form-control top-20" value="rechazado" disabled />
+      );
     }
     if (data == 2) {
       return (
-        <input
-          type="text"
-          class="form-control"
-          value='enviado'
-          disabled
-        />);
+        <input type="text" class="form-control top-20" value="enviado" disabled />
+      );
     }
     if (data == 3) {
       return (
-        <input
-          type="text"
-          class="form-control"
-          value='entregado'
-          disabled
-        />);
+        <input type="text" class="form-control top-20" value="entregado" disabled />
+      );
     }
-  }
+  };
   const Service = ({ data }) => {
     if (data == 0) {
       return (
-        <input
-          type="text"
-          class="form-control"
-          value='Entregar'
-          disabled
-        />
-      )
-    } if (data == 1) {
-      return (
-        <input
-          type="text"
-          class="form-control"
-          value='Enviar'
-          disabled
-        />
-      )
+        <input type="text" class="form-control top-20" value="Entregar" disabled />
+      );
     }
-  }
+    if (data == 1) {
+      return <input type="text" class="form-control top-20" value="Enviar" disabled />;
+    }
+  };
 
   return (
     <>
@@ -103,9 +75,7 @@ export default function Edit({ data }) {
         <CModalHeader>
           <CModalTitle id="StaticBackdropExampleLabel">
             <h2 className="title-ver-daly">Usuario: {data.user_compra}</h2>
-            <h3 className="title-ver-daly color-gray">
-              Codigo: {data.id}
-            </h3>
+            <h3 className="title-ver-daly color-gray">Codigo: {data.id}</h3>
             <h3 className="title-ver-daly color-gray">
               Fecha: {data.created_at}
             </h3>
@@ -115,9 +85,11 @@ export default function Edit({ data }) {
           <CForm className="row g-3">
             <div class="row top-50">
               <div class="col">
+                <label htmlFor="">Estado Venta:</label>
                 <Status data={data.status_venta} />
               </div>
               <div class="col">
+                <label htmlFor="">Tipo  Servicio:</label>
                 <Service data={data.tipo_servicio} />
               </div>
               <div class="col-12 top-50">
@@ -126,39 +98,51 @@ export default function Edit({ data }) {
                 <DataTable id={data.id} />
               </div>
             </div>
-          
           </CForm>
         </CModalBody>
         <CModalFooter>
-        <div class="col-12">
-              {data.tipo_servicio === 0 && (
-                <>
-                  <h2 className="title-ver-daly top-50">Factura productos:</h2>
-                  <div className="grid">
-                  <div className="img-pago">
-                    <Factura data={data} />
+          <div class="col-12">
+            {data.tipo_servicio === 0 && (
+              <>
+                <div className="grid">
+                  <div>
+                    <h2 className="title-ver-daly top-50">
+                      Factura productos:
+                    </h2>
+                    <div className="img-pago">
+                      <Factura data={data} />
+                    </div>
                   </div>
-                  <div className="img-pago">
-                    <img
-                      src={data.img}
-                      alt='Sin Comprobante de pago rechazar la solicitud¡!'
-                    />
-                  </div>
-                  </div>
-                </>
-              )}
-              {data.tipo_servicio === 1 && (
-                <>
-                  <h2 className="title-ver-daly top-50">Comprobante pago:</h2>
-                  <div className="img-pago">
-                    <img
-                      src={data.img}
-                      alt='Sin Comprobante de pago rechazar la solicitud¡!'
-                    />
-                  </div>
-                </>
-              )}
-            </div>
+                  {data.img ? (
+                    <div>
+                      <h2 className="title-ver-daly top-50">
+                        Comprobante pago:
+                      </h2>
+                      <div className="img-pago">
+                        <img
+                          src={data.img}
+                          alt="Sin Comprobante de pago rechazar la solicitud¡!"
+                        />
+                      </div>
+                    </div>
+                  ) : (
+                    ""
+                  )}
+                </div>
+              </>
+            )}
+            {data.tipo_servicio === 1 && (
+              <>
+                <h2 className="title-ver-daly top-50">Comprobante pago:</h2>
+                <div className="img-pago">
+                  <img
+                    src={data.img}
+                    alt="Sin Comprobante de pago rechazar la solicitud¡!"
+                  />
+                </div>
+              </>
+            )}
+          </div>
         </CModalFooter>
       </CModal>
     </>
