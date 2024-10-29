3a03393310cb189f0bed3498de95b9e1210b5221
flashCrisisEnTierrasInfinitas <desarrollo3@ingeal.com>
2024-10-29 18:36:15 -0500

    asd

--- src/content/pages/home/ProList.jsx
+++ src/content/pages/home/ProList.jsx
@@ -125,7 +125,8 @@ export default function ProList({ total, setTotal }) {
     <div className="margin-90 conter-pro">
       <div>
         <Categorias />
-        <DefaultCarousel data={oferta} total={total} setTotal={setTotal} />
+        {/*<DefaultCarousel data={oferta} total={total} setTotal={setTotal} /> */}
+
         <div className="mt-20 mb-10" />
         <div class="bg-white flex px-1 py-1 rounded-full border border-blue-500 overflow-hidden max-w-md mx-auto font-[sans-serif]">
           <input
