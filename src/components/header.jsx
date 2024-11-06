8134054c7cb13b6ba652c9b1fece5e57ff2c77dd
flashCrisisEnTierrasInfinitas <desarrollo3@ingeal.com>
2024-11-06 17:34:37 -0500

    nuevos cambios

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,14 +1,4 @@
 import React, { useState } from "react";
-import {
-  CNavbarNav,
-  CContainer,
-  COffcanvas,
-  COffcanvasHeader,
-  CCloseButton,
-  COffcanvasBody,
-  CNavItem,
-  CNavLink,
-} from "@coreui/react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../App.css";
 import {
@@ -16,24 +6,12 @@ import {
   LockOpenIcon,
   ShoppingCartIcon,
 } from "@heroicons/react/24/outline";
-import NotificationsActiveIcon from "@mui/icons-material/NotificationsActive";
-import LogoutIcon from "@mui/icons-material/Logout";
-import LockPersonIcon from "@mui/icons-material/LockPerson";
-import LoginIcon from "@mui/icons-material/Login";
 import { Tooltip } from "@mui/joy";
 import Cookies from "js-cookie";
-import { Avatar, Badge, Button, styled } from "@mui/material";
+import { Badge } from "@mui/material";
 import BasicMenu from "./avatars";
 
-const styles = {
-  btn: {
-    background: "#2D477C",
-  },
-};
-
 export default function Header(props) {
-  const [visible, setVisible] = useState(false);
-
   var role = Cookies.get("role");
   var seccion = Cookies.get("seccion");
   var img = Cookies.get("img");
@@ -43,8 +21,8 @@ export default function Header(props) {
     if (hastRole == 1) {
       return (
         <Tooltip title="Administrador">
-          <a href="/dahsboard/0" className="ico-navbar">
-            <LockOpenIcon className="w-6 text-gray-500" />
+          <a href="/dahsboard/0" className="text-black ">
+            <LockOpenIcon className="w-6 hover:text-gray-500" />
           </a>
         </Tooltip>
       );
@@ -76,14 +54,11 @@ export default function Header(props) {
     } else {
       return (
         <Tooltip title="Login">
-          <a href="/au5Z4YhReMcxh1r0WdbGNrGiMU7+j6CfaUrMxP2TGJNv7ZgI72muOl1gie2Lc7da">
-            <Button
-              variant="contained"
-              style={styles.btn}
-              endIcon={<LoginIcon />}
-            >
-              Ingresa
-            </Button>
+          <a
+            href="/au5Z4YhReMcxh1r0WdbGNrGiMU7+j6CfaUrMxP2TGJNv7ZgI72muOl1gie2Lc7da"
+            className="px-4 py-2 text-sm rounded-full font-bold text-white border-2 bg-blue-700 hover:bg-blue-400 flex"
+          >
+            Ingresar
           </a>
         </Tooltip>
       );
@@ -94,9 +69,9 @@ export default function Header(props) {
     if (seccion) {
       return (
         <Tooltip title="notificaciones">
-          <a href="/notificaciones" className="ico-navbar">
+          <a href="/notificaciones" className="text-black ">
             <Badge badgeContent={1} color="primary">
-              <BellIcon className="w-6  h-6 text-gray-500" />
+              <BellIcon className="w-6 hover:text-gray-500" />
             </Badge>
           </a>
         </Tooltip>
@@ -106,124 +81,124 @@ export default function Header(props) {
 
   return (
     <>
-      <div className="navbarShop">
-        <CContainer fluid>
-          <div className="conter-navbar">
-            {seccion && (
-              <div className="toogel-nav">
-                <button onClick={() => setVisible(!visible)} className="togel">
-                  <i class="fa fa-bars" aria-hidden="true"></i>
-                </button>
-              </div>
-            )}
+      <header class="shadow-md bg-white font-sans tracking-wide relative z-50">
+        <section class="flex items-center lg:justify-center flex-wrap gap-5 relative py-3 px-10 border-gray-200 border-b lg:min-h-[80px] max-lg:min-h-[60px]">
+          <a href="javascript:void(0)">
+            <img
+              src="/img/logos/logoAzul.jpeg"
+              alt="logo"
+              class="md:w-[170px] w-36"
+            />
+          </a>
 
-            <div className="footer-img">
-              <a href="/">
-                <div>
-                  <img src="/img/logos/logo1.png" className="logo-asupro" />
-                </div>
-              </a>
-            </div>
-            <div className="conter-ico-nav">
-              <Tooltip title="Configuraciones">
-                <BasicMenu role={role} img={img} seccion={seccion} />
-              </Tooltip>
-              <Admin />
-              <ValidateNotify />
-              <Tooltip title="Carrito" className="ico-navbar">
-                <a href="/7Abq5P7EeFQM7HGxE/cDsMvM5/jPk1ZIuzGwbnuVyYI+f9RUtVrP4wVLaqftHwM/">
-                  <ShoppingCartIcon className="h-6 w-6 text-gray-500" />
-                </a>
-              </Tooltip>
+          <div class="space-x-6 md:absolute md:right-10 flex items-center max-md:ml-auto">
+            <Tooltip title="Configuraciones">
+              <BasicMenu role={role} img={img} seccion={seccion} />
+            </Tooltip>
+            <Admin />
+            <ValidateNotify />
+            <a
+              href="/7Abq5P7EeFQM7HGxE/cDsMvM5/jPk1ZIuzGwbnuVyYI+f9RUtVrP4wVLaqftHwM/"
+              className="text-black "
+            >
+              <ShoppingCartIcon className="w-6 hover:text-gray-500" />
+            </a>
+            <div class="inline-block border-gray-300 border-l-2 pl-6 cursor-pointer">
               <ValidateLogin />
             </div>
           </div>
-          <COffcanvas
-            id="offcanvasNavbar"
-            portal={false}
-            visible={visible}
-            onHide={() => setVisible(false)}
-            className="position-nav"
+        </section>
+
+        <div class="flex flex-wrap py-3.5 px-10 overflow-x-auto">
+          <div
+            id="collapseMenu"
+            class="w-full max-lg:hidden lg:!block max-lg:before:fixed max-lg:before:bg-black max-lg:before:opacity-50 max-lg:before:inset-0 max-lg:before:z-50"
           >
-            <COffcanvasHeader>
-              <div className="footer-img">
-                <a href="/">
-                  <div>
-                    <img src="/img/logos/logo1.png" className="logo-asupro" />
-                  </div>
+            <button
+              id="toggleClose"
+              class="lg:hidden fixed top-2 right-4 z-[100] rounded-full bg-white p-3"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="w-4 fill-black"
+                viewBox="0 0 320.591 320.591"
+              >
+                <path
+                  d="M30.391 318.583a30.37 30.37 0 0 1-21.56-7.288c-11.774-11.844-11.774-30.973 0-42.817L266.643 10.665c12.246-11.459 31.462-10.822 42.921 1.424 10.362 11.074 10.966 28.095 1.414 39.875L51.647 311.295a30.366 30.366 0 0 1-21.256 7.288z"
+                  data-original="#000000"
+                ></path>
+                <path
+                  d="M287.9 318.583a30.37 30.37 0 0 1-21.257-8.806L8.83 51.963C-2.078 39.225-.595 20.055 12.143 9.146c11.369-9.736 28.136-9.736 39.504 0l259.331 257.813c12.243 11.462 12.876 30.679 1.414 42.922-.456.487-.927.958-1.414 1.414a30.368 30.368 0 0 1-23.078 7.288z"
+                  data-original="#000000"
+                ></path>
+              </svg>
+            </button>
+
+            <ul class="lg:flex lg:justify-center lg:gap-x-10 max-lg:space-y-3 max-lg:fixed max-lg:bg-white max-lg:w-1/2 max-lg:min-w-[300px] max-lg:top-0 max-lg:left-0 max-lg:p-6 max-lg:h-full max-lg:shadow-md max-lg:overflow-auto z-50">
+              <li class="mb-6 hidden max-lg:block">
+                <a href="javascript:void(0)">
+                  <img src="/img/logos/logoAzul.jpeg" alt="logo" class="w-36" />
                 </a>
-              </div>
-              <CCloseButton
-                className="text-reset"
-                onClick={() => setVisible(false)}
-              />
-            </COffcanvasHeader>
-            <COffcanvasBody>
-              <CNavbarNav>
-                <CNavItem>
-                  <CNavLink href="/" active>
-                    <div className="conter-nav-li">
-                      <i
-                        class="fa fa-home color-secondary"
-                        aria-hidden="true"
-                      />
-                      <label className="label-nav">Home</label>
-                    </div>
-                  </CNavLink>
-                </CNavItem>
-                <CNavItem>
-                  <CNavLink href="/Profile" active>
-                    <div className="conter-nav-li">
-                      <i
-                        class="fa fa-cog color-secondary"
-                        aria-hidden="true"
-                      ></i>
-                      <label className="label-nav">Mi perfil</label>
-                    </div>
-                  </CNavLink>
-                </CNavItem>
-                <CNavItem>
-                  <CNavLink href="/notificaciones" active>
-                    <div className="conter-nav-li">
-                      <i
-                        class="fa fa-bell color-secondary"
-                        aria-hidden="true"
-                      ></i>
-                      <label className="label-nav">Notificaciones</label>
-                    </div>
-                  </CNavLink>
-                </CNavItem>
-                <CNavItem>
-                  <CNavLink
-                    href="/Tz0QDp4ERS81rCCHx/YEWX4wTBveKdtl5szCsnv8iQv57RmjwNzB0HKzt97RBsFX"
-                    active
+              </li>
+              {Rutas.map((ruta, index) => (
+                <li class="max-lg:border-b max-lg:py-3" key={index}>
+                  <a
+                    href={ruta.path}
+                    class="hover:text-[#007bff] text-gray-500 font-bold text-[15px] block"
                   >
-                    <div className="conter-nav-li">
-                      <i
-                        class="fa fa-shopping-cart color-secondary"
-                        aria-hidden="true"
-                      />
-                      <label className="label-nav">Pedidos</label>
-                    </div>
-                  </CNavLink>
-                </CNavItem>
-              </CNavbarNav>
-            </COffcanvasBody>
-            <div className="conter-log-red">
-              <div>
-                <img src="/img/icons/facebook_108044.svg" alt="" />
-              </div>
-              <div>
-                <img src="/img/icons/instagram_108043.svg" alt="" />
-              </div>
-              <div>
-                <img src="/img/icons/whatsapp_108042.svg" alt="" />
-              </div>
-            </div>
-          </COffcanvas>
-        </CContainer>
-      </div>
+                    {ruta.label}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          <div class="flex ml-auto lg:hidden">
+            <button id="toggleOpen">
+              <svg
+                class="w-7 h-7"
+                fill="#000"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+            </button>
+          </div>
+        </div>
+      </header>
       <div>{props.children}</div>
     </>
   );
 }
+
+const Rutas = [
+  {
+    path: "/",
+    label: "Inicio",
+  },
+  {
+    path: "#",
+    label: "Categorias",
+  },
+  {
+    path: "#",
+    label: "Productos",
+  },
+  {
+    path: "/Profile",
+    label: "Mi perfil",
+  },
+  {
+    path: "/notificaciones",
+    label: "Notificaciones",
+  },
+  {
+    path: "/Tz0QDp4ERS81rCCHx/YEWX4wTBveKdtl5szCsnv8iQv57RmjwNzB0HKzt97RBsFX",
+    label: "Pedidos",
+  },
+];
